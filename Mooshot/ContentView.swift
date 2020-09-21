586910ee369c9c3dad596250f1aeb5740ff40377
Paul Houghton <paul.houghton@gmail.com>
2020-09-21 08:20:35 +0100

    Initial Commit

---
+++ Mooshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mooshot
+//
+//  Created by Paul Houghton on 21/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mooshot/MooshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooshotApp.swift
+//  Mooshot
+//
+//  Created by Paul Houghton on 21/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MooshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
